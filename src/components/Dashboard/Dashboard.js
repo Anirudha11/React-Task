4389b235842f296123775989530dd0c5ad487ef9
Anirudha11 <1405194@BCSE1405194.kiituniversity.net>
2020-09-25 17:50:33 +0530

    Added  Material Card to Dashboard UI

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,4 +1,7 @@
 import React from 'react';
+// import Card from '../UI/Card/Card';
+import Card from '@material-ui/core/Card';
+
 
 const dashboard = (props) => {
     
@@ -8,12 +11,14 @@ const dashboard = (props) => {
 
         {props.users.user.map(user=>(
             <React.Fragment key={user.id}>
-            <h4>ID:{user.id}</h4>
-            <h2>Name:{user.name}</h2>
-            <h3>Age:{user.age}</h3>
-            <h4>Email:{user.email}</h4>
-            <h4>Phone No.:{user.phoneNo}</h4>
-            <br />
+                <Card variant="outlined">
+                <h4>ID:{user.id}</h4>
+                <h2>Name:{user.name}</h2>
+                <h3>Age:{user.age}</h3>
+                <h4>Email:{user.email}</h4>
+                <h4>Phone No.:{user.phoneNo}</h4>
+                <br />
+                </Card>
         </React.Fragment>
         ))}
         </React.Fragment>
